b58512cf57f4fb908bd7fb41ae1bf1e185a0f612
anwin-kp <anwin.kp.10@gmail.com>
2023-03-03 12:49:30 +0530

    Icon Changed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
+        diceImage.contentDescription = diceRoll.toString()
         diceImage.setImageResource(drawableResource)
     }
 }
